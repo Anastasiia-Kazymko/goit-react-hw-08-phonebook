7f96277ffdbc04beebd4be9050e10b1342abf1f5
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2022-07-07 18:37:15 +0300

    add fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -48,16 +48,7 @@ export class Phonebook extends React.Component {
   };
   render() {
     return (
-      <div
-        style={{
-          height: '100vh',
-          //display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
+      <div>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
